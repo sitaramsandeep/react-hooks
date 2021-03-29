2a6db499459bb02ec80a1b5b8cea8986dcd817b2
Sandeep Potnuru <Sandeep.Potnuru@FISGLOBAL.COM>
2021-03-29 13:41:13 +0530

    useEffect basic change

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-import {FaStar} from 'react-icons/fa';
-import { useState } from 'react';
-
-const createArray = (length) => [...Array(length)]
-
-function Star({ selected = false, onSelect }) {
-  return <FaStar 
-  color={selected ? "red" : "gray"}
-  onClick={onSelect}
-  />
-}
-
-function StarRating({totalStars = 5}) {
-  const [selectedStar, setSelectedStar] = useState(0)
-
- return (
-    <>
-      {createArray(totalStars).map((n, i) => { 
-        return <Star 
-        key={i} 
-        selected={selectedStar > i} 
-        onSelect={() => setSelectedStar(i+1)}/>
-      })}
-      <p>{selectedStar } of {totalStars}</p>
-    </>
-  )
-}
 
 function App() {
-  return <StarRating totalStars={10}/>
+  const [name, setName] = useState("Jan");
+  
+  useEffect(() => {
+    document.title = `Celebrate ${name}`;
+  })
+
+  return (
+    <section>
+      <p>Congratulations {name}!</p>
+      <button onClick={() => setName("Will")}>Change Winner</button>
+    </section>
+  )
 }
 
 export default App;
