44ee9a1c3b547e0314380950f5e134afa79a03d5
Sandeep Potnuru <Sandeep.Potnuru@FISGLOBAL.COM>
2021-03-29 14:14:57 +0530

    Handling state with useReducer

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useReducer } from 'react';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch(`https://api.github.com/users`)
-    .then(response => response.json())
-    .then(setData);
-  },[])
-
-  if (data) {
-    return (
-      <div>
-        <ul>
-          {data.map((user)=> (
-            <li key={user.id}>{user.login}</li>
-          ))}
-        </ul>
-        <button onClick={() => setData([])}>Delete Users</button>
-      </div>
-    )
-  }
-
-  return <p>No Users</p>
+  const [number, setNumber] = useReducer((number, newNumber) => number + newNumber ,0)
+  return <h1 onClick={() => setNumber(1)}>{number}</h1>
 }
 
 export default App;
