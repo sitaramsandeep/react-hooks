138c68f12a70957aba155176694ce81488a01711
Sandeep Potnuru <Sandeep.Potnuru@FISGLOBAL.COM>
2021-03-29 13:02:01 +0530

    working with component tree

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import { useState } from 'react';
 import './App.css';
+import {FaStar} from 'react-icons/fa';
+
+const createArray = (length) => [...Array(length)]
+
+function Star({ selected = false }) {
+  return <FaStar color={selected ? "red" : "gray"}/>
+}
+
+function StarRating({totalStars = 5}) {
+ return createArray(totalStars).map((n, i) => { 
+   return <Star key={i}/>
+  });
+}
 
 function App() {
-  // const [status, setStatus] = useState("Not Delivered")
-  const [checked, setChecked] = useState(false);
-  return (
-    <div className="App">
-      {/* <h1>The package is:{status}</h1>
-      <button onClick={() => setStatus("Delivered")}>Deliver</button> */}
-      <input type="checkbox" value={checked} onChange={() => setChecked((checked) => !checked)}/>
-      <p>{checked ? "checked" : "not checked"}</p>
-    </div>
-  );
+  return <StarRating totalStars={10}/>
 }
 
 export default App;
