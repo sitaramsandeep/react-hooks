fa6419ecc4f28cfaa317c9dd5657435bf0fe3ea1
Sandeep Potnuru <Sandeep.Potnuru@FISGLOBAL.COM>
2021-03-29 16:29:22 +0530

    Handling state with useReducer and refactoring

--- src/App.js
+++ src/App.js
@@ -3,7 +3,17 @@ import './App.css';
 
 function App() {
   const [number, setNumber] = useReducer((number, newNumber) => number + newNumber ,0)
-  return <h1 onClick={() => setNumber(1)}>{number}</h1>
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+  )
+  return (
+    <div>
+      <h1 onClick={() => setNumber(1)}>{number}</h1>
+      <input type="checkbox" value={checked} onChange={toggle}/>
+      <p>{checked ? "checked" : "not checked"}</p>
+    </div>
+  )
 }
 
 export default App;
