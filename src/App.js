d775929ac4423ef35d4633ad05b93749f35d25d9
Sandeep Potnuru <Sandeep.Potnuru@FISGLOBAL.COM>
2021-03-29 18:30:58 +0530

    reusing form logic with customHooks

--- src/App.js
+++ src/App.js
@@ -1,39 +1,26 @@
 import { useReducer, useRef, useState } from 'react';
 import './App.css';
+import useInput from './useInput';
 
 function App() {
-  // const sound = useRef();
-  // const color = useRef();
-
-  const [sound, setSound] = useState("");
-  const [color, setColor] = useState("#000000");
+  const [titleProps, resetTitle] = useInput("");
+  const [colorProps, resetColor] = useInput("#000000");
 
   const submit = (e) => {
     e.preventDefault();
-    // const soundVal = sound.current.value;
-    // const colorVal = color.current.value;
-
-    // alert(`${soundVal} sounds like ${colorVal}`)
-    // sound.current.value = "";
-    // color.current.value = "";
-
-    alert(`${sound} sounds like ${color}`)
-    setSound("");
-    setColor("#000000");
+    alert(`${titleProps.value} sounds like ${colorProps.value}`)
+    resetTitle("");
+    resetColor("#000000");
   }
 
   return (
     <form onSubmit={submit}>
-      {/* <input ref={sound} type="text" placeholder="Sound..."/>
-      <input ref={color} type="color"/> */}
       <input type="text" 
         placeholder="Sound..."
-        value={sound}
-        onChange={(e) => setSound(e.target.value)}
+        {...titleProps}
       />
       <input type="color"
-        value={color}
-        onChange={(e) => setColor(e.target.value)}
+        {...colorProps}
       />
       <button>Add</button>
     </form>
